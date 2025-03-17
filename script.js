f7a3736192f9228d6f87fc7919970a4be5ed812b
vikram-42-alla <allatrivikram@gmail.com>
2025-03-17 20:05:28 +0530

    change

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello this is git")
+class User{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    greet(){
+        console.log(`Name ${name} Age {age}`);
+    }
+}
